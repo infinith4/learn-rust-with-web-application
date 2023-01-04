6fbe04601b0bc0b8c5cfbbed2d14580f0c843f81
infinith4 <infinith4@gmail.com>
2023-01-04 22:52:12 +0900

    Update main.rs

--- my-todo/src/main.rs
+++ my-todo/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use axum::extract::Extension;
 
 use std::net::SocketAddr;
 use std::{
@@ -106,8 +107,8 @@ async fn main() {
     let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
-    let app = create_app();
-
+    let repository = TodoRepositoryForMemory::new();
+    let app = create_app(repository);
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
  
     axum::Server::bind(&addr)
@@ -116,11 +117,22 @@ async fn main() {
         .unwrap();
 }
 
-fn create_app() -> Router{
+fn create_app<T: TodoRepository>(repository: T) -> Router{
     
     Router::new()
     .route("/", get(root))
     .route("/users", post(create_user))
+    .route("/todos", post(create_todo::<T>))
+    .layer(Extension(Arc::new(repository)))
+}
+
+pub async fn create_todo<T: TodoRepository>(
+    Json(payload): Json<CreateTodo>,
+    Extension(repository): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    let todo = repository.create(payload);
+
+    (StatusCode::CREATED, Json(todo))
 }
 
 async fn root() -> &'static str{
@@ -180,8 +192,9 @@ mod test{
         .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
         .body(Body::from(r#"{ "username": "田中 太郎" }"#))
         .unwrap();
+        let repository = TodoRepositoryForMemory::new();
 
-        let res = create_app().oneshot(req).await.unwrap();
+        let res = create_app(repository).oneshot(req).await.unwrap();
         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
 
         let body: String = String::from_utf8(bytes.to_vec()).unwrap();
