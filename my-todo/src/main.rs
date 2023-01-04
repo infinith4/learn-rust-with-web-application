924ec47388d9ebc92a9f38f370bb60f85f02f4db
infinith4 <infinith4@gmail.com>
2023-01-04 22:37:50 +0900

    Update main.rs

--- my-todo/src/main.rs
+++ my-todo/src/main.rs
@@ -64,6 +64,12 @@ impl Todo {
     }
 }
 
+type TodoDatas = HashMap<i32, Todo>;
+
+#[derive(Debug, Clone)]
+pub struct TodoRepositoryForMemory {
+    store: Arc<RwLock<TodoDatas>>,
+}
 
 impl TodoRepositoryForMemory {
     pub fn new() -> Self {
@@ -71,53 +77,27 @@ impl TodoRepositoryForMemory {
             store: Arc::default(),
         }
     }
-
-    fn write_store_ref(&self) -> RwLockWriteGuard<TodoDatas> {
-        self.store.write().unwrap()
-    }
-
-    fn read_store_ref(&self) -> RwLockReadGuard<TodoDatas> {
-        self.store.read().unwrap()
-    }
 }
 
 impl TodoRepository for TodoRepositoryForMemory {
     fn create(&self, payload: CreateTodo) -> Todo {
-        let mut store = self.write_store_ref();
-        let id = (store.len() + 1) as i32;
-        let todo = Todo::new(id, payload.text.clone());
-        store.insert(id, todo.clone());
-        todo
+        todo!();
     }
 
     fn find(&self, id: i32) -> Option<Todo> {
-        let store = self.read_store_ref();
-        store.get(&id).map(|todo| todo.clone())
+        todo!();
     }
 
     fn all(&self) -> Vec<Todo> {
-        let store = self.read_store_ref();
-        Vec::from_iter(store.values().map(|todo| todo.clone()))
+        todo!();
     }
 
     fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
-        let mut store = self.write_store_ref();
-        let todo = store.get(&id).context(RepositoryError::NotFound(id))?;
-        let text = payload.text.unwrap_or(todo.text.clone());
-        let completed = payload.completed.unwrap_or(todo.completed);
-        let todo = Todo {
-            id,
-            text,
-            completed,
-        };
-        store.insert(id, todo.clone());
-        Ok(todo)
+        todo!();
     }
 
     fn delete(&self, id: i32) -> anyhow::Result<()> {
-        let mut store = self.write_store_ref();
-        store.remove(&id).ok_or(RepositoryError::NotFound(id))?;
-        Ok(())
+        todo!();
     }
 }
 
